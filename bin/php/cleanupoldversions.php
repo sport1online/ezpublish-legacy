cfd80f56a0435b50a64807e59bcede2e9a6f0e09
s1-cwirkus <christian.wirkus@sport1.de>
2017-02-01 16:59:14 +0100

    Update cleanupoldversions.php

--- bin/php/cleanupoldversions.php
+++ bin/php/cleanupoldversions.php
@@ -9,7 +9,7 @@ $cli = eZCLI::instance();
 
 $script = eZScript::instance(
     array(
-        'description' => 'This script will progressively cleanup history, 1000 objects at a time.',
+        'description' => 'This script will progressively cleanup history, 2000 objects at a time.',
         'use-session' => false,
         'use-modules' => true,
         'use-extensions' => true,
@@ -29,7 +29,7 @@ $rows = $db->arrayQuery('
     where created < unix_timestamp(now() - interval 2 month)
     group by contentobject_id
     having count > 1
-    limit 1000
+    -- limit 2000
     ;
 ');
 
@@ -37,25 +37,14 @@ foreach ($rows as $row) {
     $cli->output($row['contentobject_id']);
 
     $object = eZContentObject::fetch($row['contentobject_id']);
-    $versionCount = $object->getVersionCount();
-
-    if ($versionCount < 2) {
-        continue;
-    }
-
-    $versionToRemove = $versionCount - 1;
     $versions = $object->versions(true, array(
         'conditions' => array('status' => \eZContentObjectVersion::STATUS_ARCHIVED),
-        'sort' => array('modified' => 'asc'),
-        'limit' => array('limit' => $versionToRemove, 'offset' => 0),
     ));
 
     $db->begin();
-
     foreach ($versions as $version) {
         $version->removeThis();
     }
-
     $db->commit();
 }
 
