4cd769438e0e2c1e031debdfbd9df46d6b9856c7
s1-cwirkus <christian.wirkus@sport1.de>
2017-02-03 15:45:36 +0100

    Do not delete entirely and clear cache

--- bin/php/cleanupoldversions.php
+++ bin/php/cleanupoldversions.php
@@ -1,20 +1,18 @@
 #!/usr/bin/env php
 <?php
 
-// ezpublish/console ezpublish:legacy:script bin/php/cleanupoldversions.php -n
+// ezpublish/console ezpublish:legacy:script bin/php/cleanupoldversions.php
 
 require_once 'autoload.php';
 
 $cli = eZCLI::instance();
 
-$script = eZScript::instance(
-    array(
-        'description' => 'This script will progressively cleanup history, 2000 objects at a time.',
-        'use-session' => false,
-        'use-modules' => true,
-        'use-extensions' => true,
-    )
-);
+$script = eZScript::instance(array(
+    'description' => 'This script will progressively cleanup history, 2000 objects at a time.',
+    'use-session' => false,
+    'use-modules' => true,
+    'use-extensions' => true,
+));
 
 $script->startup();
 $script->initialize();
@@ -29,23 +27,41 @@ $rows = $db->arrayQuery('
     where created < unix_timestamp(now() - interval 2 month)
     group by contentobject_id
     having count > 1
-    -- limit 2000
+    limit 2000
     ;
 ');
 
-foreach ($rows as $row) {
-    $cli->output($row['contentobject_id']);
-
+$length = count($rows);
+foreach ($rows as $index => $row) {
     $object = eZContentObject::fetch($row['contentobject_id']);
-    $versions = $object->versions(true, array(
-        'conditions' => array('status' => \eZContentObjectVersion::STATUS_ARCHIVED),
-    ));
+    $class = $object->attribute('content_class')->Identifier;
 
-    $db->begin();
-    foreach ($versions as $version) {
-        $version->removeThis();
+    if (0 === strpos($class, 'sport1_')) {
+        $cli->output(sprintf(
+            "%s\t%s\t%s\t%s",
+            $row['contentobject_id'],
+            $class,
+            $index + 1,
+            $length
+        ));
+
+        $versions = $object->versions(true, array(
+            'conditions' => array('status' => \eZContentObjectVersion::STATUS_ARCHIVED),
+            'limit' => array(
+                'limit' => $object->getVersionCount() - 1,
+                'offset' => 0,
+            ),
+            'sort' => array('modified' => 'asc'),
+        ));
+
+        $db->begin();
+        foreach ($versions as $version) {
+            $version->removeThis();
+        }
+        $db->commit();
+
+        eZContentObject::clearCache();
     }
-    $db->commit();
 }
 
 $script->shutdown();
